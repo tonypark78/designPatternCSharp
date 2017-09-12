7a94b99772ec11e5145e100fdee2ba7a57b2497b
Tony Park <tonypark0403@gmail.com>
2017-09-11 23:05:27 -0400

    implement basic solid design

--- DS/Program.cs
+++ DS/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DS
 {
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public static Journal Load(string filename)
+        {
+            
+        }
+
+        public void Load(Uri uri)
+        {
+            
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var j = new Journal();
+            j.AddEntry("I cried today");
+            j.AddEntry("I ate a bug");
+            Console.WriteLine(j);
         }
     }
 }
